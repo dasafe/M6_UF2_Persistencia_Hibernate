0774227213948aec1494e1ea924edd908bc6fb1c
David Salas <dsf.2528@gmail.com>
2020-01-13 18:39:28 +0100

    commit

--- src/main/java/persistencia/HibernateManager.java
+++ src/main/java/persistencia/HibernateManager.java
@@ -1,5 +1,7 @@
 package persistencia;
 
+import java.util.Scanner;
+
 import org.hibernate.Session;
 
 import biblioteca.Llibres;
@@ -8,15 +10,38 @@ public class HibernateManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+
 		Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        Llibres libro = new Llibres();
-        libro.setEditoria("Edito");
-        libro.setNomLlibre("HLibrito");
-        session.save(libro);
-        session.getTransaction().commit();
-        HibernateUtil.getSessionFactory().close();
-        System.out.println("Base de datos actualizada");
+		session.beginTransaction();
+		System.out.println("Crearemos, modificaremos y eliminaremos un libro.");
+		Llibres libro = new Llibres();
+		System.out.println("Editorial del libro:");
+		String editorial = reader.nextLine();
+		System.out.println("Nombre del libro:");
+		String nombre = reader.nextLine();
+		libro.setEditoria(editorial);
+		libro.setNomLlibre(nombre);
+		session.save(libro);
+		session.getTransaction().commit();
+		System.out.println("Libro creado.");
+		
+		session.beginTransaction();
+		System.out.println("\nAhora modificamos el nombre del libro.");
+		System.out.println("Nuevo nombre del libro:");
+		nombre = reader.nextLine();
+		libro.setNomLlibre(nombre);
+		session.update(libro);
+		session.getTransaction().commit();
+		
+		session.beginTransaction();
+		System.out.println("\nAhora eliminamos el libro.");
+		session.remove(libro);
+		session.getTransaction().commit();
+		System.out.println("Libro eliminado.");
+		
+		HibernateUtil.getSessionFactory().close();
+		System.out.println("Base de datos actualizada");
 	}
 
 }
